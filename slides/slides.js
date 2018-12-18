bd648edc91ce77f64d5cb71f027496e365b35c8c
El Sadate <aelsadate@gmail.com>
2018-12-18 10:18:58 -0500

    slides updated

--- slides/slides.js
+++ slides/slides.js
@@ -1,14 +1,56 @@
-function setup() {
-  createCanvas(400, 400);
-  var cnv = createCanvas(800, 600);
-  var x = (windowWidth - width) / 2;
-  var y = (windowHeight - height) / 2;
-// cnv.position(x, y);
-  background(255, 255, 255);
+/*
+    final project presentation slides
+*/
+var slide = 0;
 
+var bird, fish;
+
+function preload() {
+    baby = loadImage("baby.png");
+    grave = loadImage("gravestone.png");
+    sensor = loadImage("potentiometer.jpg");
+    
+}
+
+function setup() {
+    createCanvas(800, 600);
+    textSize(60);
+    textFont('Playfair Display');
+    textAlign(CENTER, CENTER);
+    rectMode(CENTER);
 }
 
 function draw() {
+    background(255, 255, 255);
 
+    if (slide == 0) {
+        text("Final Project Idea", width/2, height/2);
+    } else if (slide == 1) {
+        textSize(30);
+        text("Potentiometer", 200, 100);
 
+        // potientiometer image
+        image(sensor, 400, 200);
+    } else if (slide == 2) {
+        text("theme: Human Evolution", 200, 40);
+        image(baby, 200, 100);
+        image(grave, 400, 100);
+    } else if (slide == 3) {
+        text("The potentiometer will control the human evolution throughout time.", width/2, height/2);
+        textSize(20);
+    }
+    
+    
+}
+
+function mousePressed() {
+    if (mouseX > width/2) {
+        if (slide < 3) {
+            slide++;
+        }
+    } else {
+        if (slide > 0) {
+            slide--;
+        }
+    }
 }
